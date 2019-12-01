ca6a3569c138035f4377ef09c2d7d183edb135be
paul wisehart <paul@oldcode.org>
2019-12-01 05:10:59 -0500

    asd

--- src/myrec_pdf/core.clj
+++ src/myrec_pdf/core.clj
@@ -1,7 +1,8 @@
-(ns myrec-pdf.core
-  (:gen-class))
+(ns myrec-pdf.core (:gen-class))
+
+(def pdf-fn "9e9ed3_3dc0b7e52b1d4203b7d673dc95224db9.pdf")
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+	"I don't do a whole lot ... yet."
+	[& args]
+	(println (str "Hello, World!: " pdf-fn)))
