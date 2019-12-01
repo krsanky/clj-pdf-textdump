0d0475d19cce7f5ca7a160aa92289aa94200009d
paul wisehart <paul@oldcode.org>
2019-12-01 05:05:55 -0500

    lein new app myrec-pdf

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to myrec-pdf
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/myrec_pdf/core_test.clj
@@ -0,0 +1,7 @@
+(ns myrec-pdf.core-test
+  (:require [clojure.test :refer :all]
+            [myrec-pdf.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
