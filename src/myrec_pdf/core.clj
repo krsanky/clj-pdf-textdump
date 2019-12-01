eb6abd22ab87714bd9d30d9e333b1cf41d036975
paul wisehart <paul@oldcode.org>
2019-12-01 05:18:00 -0500

    asd

--- src/myrec_pdf/core.clj
+++ src/myrec_pdf/core.clj
@@ -1,4 +1,6 @@
-(ns myrec-pdf.core (:gen-class))
+(ns myrec-pdf.core 
+	(:gen-class)
+	(:require [pdfboxing.text :as text]))
 
 (def pdf-fn "9e9ed3_3dc0b7e52b1d4203b7d673dc95224db9.pdf")
 
@@ -6,3 +8,8 @@
 	"I don't do a whole lot ... yet."
 	[& args]
 	(println (str "Hello, World!: " pdf-fn)))
+
+
+
+;;(require '[pdfboxing.text :as text])
+;;(text/extract "test/pdfs/hello.pdf")
